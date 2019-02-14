3894b85e54033037c33bee3c469d02835d55bcb5
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-14 16:16:16 -0500

    started checklist

--- App.js
+++ App.js
@@ -1,4 +1,70 @@
+const data = [
+    {
+        title: "Sporting Goods",
+        items:[
+            {
+                name: "Football",
+                price: "$49.99"
+            },
+            {
+                name: "Baseball",
+                price: "$9.99"
+            },
+            {
+                name: "Basketball",
+                price: "$29.99"
+            }
+        ]
+    },
+    {
+        title: "Electronics",
+        items:[
+            {
+                name: "iPod Touch",
+                price: "$99.99"
+            },
+            {
+                name: "iPhone 5",
+                price: "$399.99"
+            },
+            {
+                name: "Nexus 7",
+                price: "$199.99"
+            }
+        ]
+    },
+]
+
+class SearchBar extends React.Component{
+    render(){
+        return(
+        <div className="searchBar">
+            <form>
+                <input type="text" placeholder="Search..."/>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+        )
+    }
+}
+
+class ProductTable extends React.Component{
+    
+}
+
+
+class FilterableProductTable extends React.Component{
+    render(){
+        return (
+            <div className="container">
+                <SearchBar/>
+                <ProductTable/>
+            </div>
+        )
+    }
+}
+
 ReactDOM.render(
-    <h1>Sanity Check</h1>,
+    <FilterableProductTable/>,
     document.getElementById("root")
 )
