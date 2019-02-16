7773e89109a05c02bfc51f3b117a61a3aecbc447
seanmcquaid <sean.mcquaid2@verizon.net>
2019-02-16 14:31:14 -0500

    finished checklist exercise?

--- App.js
+++ App.js
@@ -35,3 +35,73 @@ const data = [
     },
 ]
 
+
+class SearchBar extends React.Component {
+    render(){
+        return(
+            <div className="searchBar">
+                <form>
+                    <input type="text" name="userInput" placeholder="Search..."/>
+                    <div className="checkbox">
+                        <input type="checkbox" id="productCheckbox"/>
+                        <label for="productCheckbox">Only show products in stock</label>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+class ProductRow extends React.Component {
+    render(){
+        const productInfo = this.props.items.map((item)=>{
+            return(
+                <div className="itemInfo">
+                    <span className="productName">{item.name}</span>
+                    <span className="productPrice">{item.price}</span>
+                </div>
+            )
+        })
+        return(
+            <div className="productRow">
+                <h2>{this.props.title}</h2>
+                {productInfo}
+            </div>
+        )
+    }
+}
+
+class ProductTable extends React.Component {
+    render(){
+        // console.log(data)
+        const products = data.map((product)=>{
+            return(
+            <ProductRow title={product.title} items={product.items}/>
+            )
+        })
+        return(
+            <div className="productTable">
+                <div className="row">
+                <h4><span>Name</span><span>Price</span></h4>
+                </div>
+                {products}
+            </div>
+        )
+    }
+}
+
+class FilterableProductTable extends React.Component{
+    render(){
+        return(
+            <div className="container">
+                <SearchBar/>
+                <ProductTable/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <FilterableProductTable/>,
+    document.getElementById("root")
+)
